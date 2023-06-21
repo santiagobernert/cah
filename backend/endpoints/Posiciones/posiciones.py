de5ce4b345023e0992bd504c2e69c96a8677a4c8
santiagobernert <s.bernert@alumno.etec.um.edu.ar>
2023-06-21 11:53:07 -0300

    added complete table detail to posiciones

--- backend/endpoints/Posiciones/posiciones.py
+++ backend/endpoints/Posiciones/posiciones.py
@@ -28,23 +28,42 @@ class Tabla:
 
         print(self.equipos)
         for e in self.equipos:
-            self.posiciones[e] = 0
+            self.posiciones[e] = {"J":0, "G":0, "P":0, "E":0, "GF":0, "GC":0, "DG":0, "PTS":0, }
 
         for p in partidos:
-            if p.resultado[:2] > p.resultado[-2:]:
-                self.posiciones[p.equipoA] += 3
-                self.posiciones[p.equipoB] += 1
-            elif p.resultado[:2] < p.resultado[-2:]:
-                self.posiciones[p.equipoB] += 3
-                self.posiciones[p.equipoA] += 1
+            self.posiciones[p.equipoA]["J"] += 1
+            self.posiciones[p.equipoB]["J"] += 1
+            # sum goal difference
+            self.posiciones[p.equipoA]["GF"] += int(p.resultado.split("-")[0])
+            self.posiciones[p.equipoB]["GF"] += int(p.resultado.split("-")[1])
+            self.posiciones[p.equipoA]["GC"] += int(p.resultado.split("-")[1])
+            self.posiciones[p.equipoB]["GC"] += int(p.resultado.split("-")[0])
+            self.posiciones[p.equipoA]["DG"] = self.posiciones[p.equipoA]["GF"]-self.posiciones[p.equipoA]["GC"]
+            self.posiciones[p.equipoB]["DG"] = self.posiciones[p.equipoB]["GF"]-self.posiciones[p.equipoB]["GC"]
+            # sum points to winner and loser
+            if p.resultado.split("-")[0] > p.resultado.split("-")[1]:
+                self.posiciones[p.equipoA]["PTS"] += 3
+                self.posiciones[p.equipoA]["G"] += 1
+                self.posiciones[p.equipoB]["PTS"] += 1
+                self.posiciones[p.equipoB]["P"] += 1
+            elif p.resultado.split("-")[0] < p.resultado.split("-")[1]:
+                self.posiciones[p.equipoB]["PTS"] += 3
+                self.posiciones[p.equipoA]["G"] += 1
+                self.posiciones[p.equipoA]["PTS"] += 1
+                self.posiciones[p.equipoB]["P"] += 1
             else:
-                self.posiciones[p.equipoA] += 2
-                self.posiciones[p.equipoB] += 2
+                self.posiciones[p.equipoA]["PTS"] += 2
+                self.posiciones[p.equipoB]["PTS"] += 2
+                self.posiciones[p.equipoA]["E"] += 1
+                self.posiciones[p.equipoB]["E"] += 1
 
     def str(self):
-        posiciones = [(v, k) for k,v in self.posiciones.items()]
-        posiciones = sorted(posiciones, reverse=True)
-        posiciones = {k:v for v,k in posiciones}
+        # turning into list (points, name)
+        pts = [(v["PTS"], k) for k,v in self.posiciones.items()]
+        # sorting by points
+        order = sorted(pts, reverse=True)
+        # turning to dict again
+        posiciones = {k:self.posiciones[k] for v,k in order}
         return posiciones
 
             
